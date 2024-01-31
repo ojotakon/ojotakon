3c9000d1ef7ca0768c2070a4250665ee56e5b9de
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-01-31 14:55:18 +0700

    Update ojotakon.py

--- ojotakon.py
+++ ojotakon.py
@@ -20,3 +20,37 @@ ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig)
+
+# subheader
+st.subheader("plot sin & cos")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.caption('Plot Sin')
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+  y = np.sin(x)
+
+  fig, ax = plt.subplots(figsize = (16, 8))
+  ax.plot(x, y, label='sin(x)', color='b')
+  ax.set_ylabel("Sin x")
+  ax.set_xlabel("x")
+  ax.tick_params(axis='y', labelsize=20)
+  ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+  ax.tick_params(axis='x', labelsize=15)
+
+  st.pyplot(fig)
+with col2:
+  st.caption('Plot Cos')
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+  y = np.cos(x)
+
+  fig, ax = plt.subplots(figsize = (16, 8))
+  ax.plot(x, y, label='cos(x)', color='b')
+  ax.set_ylabel("Cos x")
+  ax.set_xlabel("x")
+  ax.tick_params(axis='y', labelsize=20)
+  ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+  ax.tick_params(axis='x', labelsize=15)
+
+  st.pyplot(fig)
