dff7a63bf555f17bfa203628e6c5ae400d0cb287
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-01-31 14:13:49 +0700

    Create ojotakon.py

---
+++ ojotakon.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlip.pyplot as plt
+import streamlit as st
+
+# --Header--
+st.header('ojotakon : sparkles')
+st.subheader('plot')
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) 
+y = np.sin(x)
+z = np.cos(x)
+
+fig, ax = plt.subplot(figsize=(16, 8))
+ax.plot(x, y label='sin(x)', color='b')
+ax.plot(x, y label='cos(x)', color='g')
+ax.set_ylabel("")
+ax.set_xlabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xlicklabels(ax.get_sticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
