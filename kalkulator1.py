3c98f244b8f9b91d965788a0457b89e0de5558b9
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-07 14:07:53 +0700

    Update kalkulator1.py

--- kalkulator1.py
+++ kalkulator1.py
@@ -11,7 +11,7 @@ with c1:
 with c2:
     operan = st.selectbox(
       'Operan',
-      ('+', '-', ':', 'x'),key='k1'
+      ('+', '-', ':', 'x'),key='k1')
 with c3:
   y = st.number_input('y ', value=0)
 with c4:
