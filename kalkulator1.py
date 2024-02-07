027e330a9f91e29be37cc3d86148b3464f2cdbcb
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-07 14:34:03 +0700

    Update kalkulator1.py

--- kalkulator1.py
+++ kalkulator1.py
@@ -15,7 +15,7 @@ with c2:
 with c3:
   y = st.number_input('y ', value=0)
 with c4:
-  z = st.number_result('z ')
+  z = st.number_result('z ', '=')
   if(operan=='+'):
     st.write('= ', x+y)
   if(operan=='-'):
