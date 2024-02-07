c8d1174d6982703aa92e70554839bbbebf47e3a3
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-07 14:04:52 +0700

    Update kalkulator1.py

--- kalkulator1.py
+++ kalkulator1.py
@@ -7,13 +7,13 @@ st.subheader('Plot')
 c1, c2, c3, c4 = st.columns(4)
 
 with c1:
-  x = st.number_input('x ', value=100)
+  x = st.number_input('x ', value=0)
 with c2:
     operan = st.selectbox(
       'Operan',
       ('+', '-', ':', 'x')
 with c3:
-  y = st.number_input('y ', value=100)
+  y = st.number_input('y ', value=0)
 with c4:
   z = st.number_input('hasil ', value=100)
   st.write('=>: ')
