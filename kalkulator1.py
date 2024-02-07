1cac3a1996e8cf1712cb87eadf4437ebf98101bc
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-07 14:48:56 +0700

    Update kalkulator1.py

--- kalkulator1.py
+++ kalkulator1.py
@@ -16,13 +16,16 @@ with c3:
   y = st.number_input('y ', value=0)
 with c4:
   if(operan=='+'):
-    st.write('= ', x+y)
+    st.write('hasil ')
+    st.write('= ', x+y) 
   if(operan=='-'):
+    st.write('hasil ')
     st.write('= ', x-y)
   if(operan==':'):
+    st.write('hasil ')
     st.write('= ', x/y)
   if(operan=='x'):
+    st.write('hasil ')
     st.write('= ', x*y)
- z = st.write('kolom ')
 
 st.write(x, ' ', operan, y, ' ', '=', ' ')
