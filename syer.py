e24947a1d655f30ae3f3a5ca7351c557e1c04e47
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-21 14:12:56 +0700

    Update syer.py

--- syer.py
+++ syer.py
@@ -2,6 +2,24 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+def f(x):
+  a = 19
+  b = 25
+  c = 5
+  return a*x+b*x-c
+u = f(t)
+#st.write('nilai t:', t)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(t, u, label='sin(t)', color='b')
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)
+
 x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
 y = st.slider('Set nilai',0.0, 10.0, 6.0)
