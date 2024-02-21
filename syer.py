1ab0fc18d3c88e87e44cd7a5d49a2795260478be
ojotakon <158257021+ojotakon@users.noreply.github.com>
2024-02-21 14:14:46 +0700

    Update syer.py

--- syer.py
+++ syer.py
@@ -2,12 +2,13 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
-  a = 19
-  b = 25
-  c = 5
-  return a*x+b*x-c
-u = f(t)
+x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
+st.write('nilai x:', x)
+y = st.slider('Set nilai',0.0, 10.0, 6.0)
+st.write('nilai y:', y)
+
+t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
+u = np.sin(y*t)
 #st.write('nilai t:', t)
 
 fig, ax = plt.subplots(figsize=(16, 8))
@@ -20,15 +21,13 @@ ax.tick_params(axis='x', labelsize=15)
 plt.grid(color='green', linestyle='-.', linewidth=.5)
 st.pyplot(fig)
 
-x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
-st.write('nilai x:', x)
-y = st.slider('Set nilai',0.0, 10.0, 6.0)
-st.write('nilai y:', y)
-
-t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
-u = np.sin(y*t)
-#st.write('nilai t:', t)
-
+def f(x):
+  a = 19
+  b = 25
+  c = 5
+  return a*x+b*x-c
+  
+u = f(t)
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(t, u, label='sin(t)', color='b')
 ax.set_ylabel("")
